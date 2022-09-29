c08779943085fdb1b1a09e1d138eba81cb38f0c9
miss-mad <mmlh.harris@gmail.com>
2022-09-28 23:06:03 -0400

    auto_increment in sql files

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE business_db;
 USE business_db;
 
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
@@ -19,14 +19,12 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT, FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-
-
 
 -- See database in use --
 SELECT DATABASE();
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,32 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES
-    ( 1, "Engineering"),
-    ( 2, "Finance"),
-    ( 3, "Legal"),
-    ( 4, "Sales"),
-    ( 5, "Service");
-    -- ( 6, "HR");
+    ("Engineering"),
+    ("Finance"),
+    ("Legal"),
+    ("Sales"),
+    ("Service");
 
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
-    ( 1, "Sales Lead", 100000, 4),
-    ( 2, "Salesperson", 80000, 4),
-    ( 3, "Lead Engineer", 150000, 1),
-    ( 4, "Software Engineer", 120000, 1),
-    ( 5, "Account Manager", 160000, 2),
-    ( 6, "Accountant", 125000, 2),
-    ( 7, "Legal Team Lead", 250000, 3),
-    ( 8, "Lawyer", 190000, 3),
-    ( 9, "Customer Service", 60000, 5);
+    ("Sales Lead", 100000, 4),
+    ("Salesperson", 80000, 4),
+    ("Lead Engineer", 150000, 1),
+    ("Software Engineer", 120000, 1),
+    ("Account Manager", 160000, 2),
+    ("Accountant", 125000, 2),
+    ("Legal Team Lead", 250000, 3),
+    ("Lawyer", 190000, 3),
+    ("Customer Service", 60000, 5);
     -- ( 10, "HR Representative");
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ( 1, "John", "Doe", 1, null),
-    ( 2, "Mike", "Chan", 2, 1),
-    ( 3, "Ashley", "Rodriguez", 3, null),
-    ( 4, "Kevin", "Tupik", 4, 3),
-    ( 5, "Kunal", "Singh", 5, null),
-    ( 6, "Malia", "Brown", 6, 5),
-    ( 7, "Sarah", "Lourd", 7, null),
-    ( 8, "Tom", "Allen", 8, 7),
-    ( 9, "Jason", "Frello", 9, 2);
+    ("John", "Doe", 1, null),
+    ("Mike", "Chan", 2, 1),
+    ("Ashley", "Rodriguez", 3, null),
+    ("Kevin", "Tupik", 4, 3),
+    ("Kunal", "Singh", 5, null),
+    ("Malia", "Brown", 6, 5),
+    ("Sarah", "Lourd", 7, null),
+    ("Tom", "Allen", 8, 7),
+    ("Jason", "Frello", 9, 2);
